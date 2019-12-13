256f7ac06f5ac38acfec48277ab2d9cc42ba353f
Dimitar Mitkov <mitkov.dimitar@yahoo.com>
2019-12-13 12:44:00 +0200

    modal

---
+++ main.js
@@ -0,0 +1,27 @@
+//smooth scroll
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 1700
+});
+
+//modal
+//display modal 
+
+const button = document.querySelectorAll('.cta');
+const modalBg = document.querySelector('.modal-bg');
+const modalClose = document.querySelector('.modal-close');
+const modalContent = document.querySelector('.modal');
+
+button.forEach((button)=>{
+   button.addEventListener('click', ()=>{
+       modalBg.classList.add('modal-visible');
+       document.querySelector('body').style.position = 'fixed';
+       modalContent.style.animation = 'modalanimation 400ms ease-in'
+   })
+})
+
+modalClose.addEventListener('click', ()=>{
+    modalBg.classList.remove('modal-visible');
+    document.querySelector('body').style.position = 'static';
+    modalContent.style.animation = '';
+})
