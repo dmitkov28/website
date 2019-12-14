ffa882a8397888f830cdc1c1171e5a00e0df9329
Dimitar Mitkov <mitkov.dimitar@yahoo.com>
2019-12-14 18:44:26 +0200

    vh test

--- main.js
+++ main.js
@@ -29,6 +29,8 @@ modalClose.addEventListener('click', ()=>{
 //slider
 new Glider(document.querySelector('.glider'), {
     slidesToShow: 1,
+    slidesToScroll: 1,
+    scrollLock: true,
     dots: '.dots',
     draggable: true,
     arrows: {
