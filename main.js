cf629d289881c7a7bb6735dd278a80cc7b90023d
Dimitar Mitkov <mitkov.dimitar@yahoo.com>
2019-12-16 12:44:39 +0200

    vh test

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-// $(function() {
-//           $.scrollify({
-//             section : ".section",
-//             sectionName : "section-name",
-//             interstitialSection : "",
-//             easing: "easeOutExpo",
-//             scrollSpeed: 1100,
-//             offset : 0,
-//             scrollbars: true,
-//             standardScrollElements: "",
-//             setHeights: true,
-//             overflowScroll: true,
-//             updateHash: true,
-//             touchScroll:true,
-//             before:function() {},
-//             after:function() {},
-//             afterResize:function() {},
-//             afterRender:function() {}
-//           });
-//         });
+$(function() {
+          $.scrollify({
+            section : ".section",
+            sectionName : "section-name",
+            interstitialSection : "",
+            easing: "easeOutExpo",
+            scrollSpeed: 1100,
+            offset : 0,
+            scrollbars: true,
+            standardScrollElements: "",
+            setHeights: false,
+            overflowScroll: true,
+            updateHash: true,
+            touchScroll:true,
+            before:function() {},
+            after:function() {},
+            afterResize:function() {},
+            afterRender:function() {}
+          });
+        });
  
 //smooth scroll
 
